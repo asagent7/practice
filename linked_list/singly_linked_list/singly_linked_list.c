1282160aaa27ec4fe955fbc2373f7d3e5e6e52db
Akshit Sharma <asagent7@gmail.com>
2016-03-23 01:26:53 +0530

    Added functions to insert at start, and print recursively.

--- linked_list/singly_linked_list/singly_linked_list.c
+++ linked_list/singly_linked_list/singly_linked_list.c
@@ -9,15 +9,20 @@ typedef struct node
 
 node* create(int insert_value);
 void destroy(node* del_node);
+void insert_start(node** head, int insert_value);
+void print_list(node* head);
 
 int main()
 {
-    node* first = NULL;
-    first = create(5);
-    if (first != NULL)
+    node* head = NULL;
+    insert_start(&head, 5);
+    if (head != NULL)
     {
-        printf("%d\n", first->value);
-        destroy(first);
+        print_list(head);
+        printf("Inserting new value...\n");
+        insert_start(&head, 7);
+        print_list(head);
+        destroy(head);
         return 0;
     }
     else
@@ -29,7 +34,7 @@ int main()
 
 node* create(int insert_value)
 {
-    node* new = malloc(sizeof(node));
+    node* new = (node*) malloc(sizeof(node));
 
     if (new != NULL)
     {
@@ -49,5 +54,30 @@ void destroy(node* del_node)
     {
         destroy(del_node->ptr);
         free(del_node);
+    }
+}
+
+void insert_start(node** head, int insert_value)
+{
+    node* new = (node*) malloc(sizeof(node));
+
+    if (new != NULL)
+    {
+        new->value = insert_value;
+        new->ptr = *head;
+        *head = new;
+    }
+}
+
+void print_list(node* head)
+{
+    if (head != NULL)
+    {
+        printf("%d ", head->value);
+        print_list(head->ptr);
+    }
+    else
+    {
+        printf("\n");
     }
 }
