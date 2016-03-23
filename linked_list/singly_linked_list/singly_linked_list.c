5ef4a7fd6e9a5b19ed9af8c4a411aca999a9e5d3
Akshit Sharma <asagent7@gmail.com>
2016-03-23 12:47:19 +0530

    Added functions to delete a node, create a sorted list.

--- linked_list/singly_linked_list/singly_linked_list.c
+++ linked_list/singly_linked_list/singly_linked_list.c
@@ -11,16 +11,28 @@ node* create(int insert_value);
 void destroy(node* del_node);
 void insert_start(node** head, int insert_value);
 void print_list(node* head);
+void insert_end(node** head, int insert_value);
+void insert_sort(node** head, int insert_value);
+void delete_node(node** head, int delete_value);
 
 int main()
 {
     node* head = NULL;
-    insert_start(&head, 5);
+    insert_sort(&head, 5);
     if (head != NULL)
     {
         print_list(head);
         printf("Inserting new value...\n");
-        insert_start(&head, 7);
+        insert_sort(&head, 6);
+        print_list(head);
+        printf("Inserting new value...\n");
+        insert_sort(&head, 4);
+        print_list(head);
+        //printf("Inserting new value...\n");
+        //insert_sort(&head, 6);
+        //print_list(head);
+        printf("Deleting value 6...\n");
+        delete_node(&head, 6);
         print_list(head);
         destroy(head);
         return 0;
@@ -66,6 +78,92 @@ void insert_start(node** head, int insert_value)
         new->value = insert_value;
         new->ptr = *head;
         *head = new;
+    }
+}
+
+void insert_end(node** head, int insert_value)
+{
+    node* crawler;
+    crawler = *head;
+    while (crawler->ptr != NULL)
+    {
+        crawler = crawler->ptr;
+    }
+
+    node* new = (node*) malloc(sizeof(node));
+
+    if (new != NULL)
+    {
+        new->value = insert_value;
+        new->ptr = NULL;
+        crawler->ptr = new;
+    }
+}
+
+void insert_sort(node** head, int insert_value)
+{
+    node* new;
+    node* crawler = *head;
+    node* precrawler = NULL;
+
+    if (crawler != NULL)
+    {
+        while (crawler->value < insert_value)
+        {
+            precrawler = crawler;
+            crawler = crawler->ptr;
+            if(crawler == NULL)
+            {
+                break;
+            }
+        }
+    }
+
+    if(precrawler == NULL)
+    {
+        new = (node*) malloc(sizeof(node));
+        new->value = insert_value;
+        new->ptr = *head;   // NULL
+        *head = new;
+    }
+    else
+    {
+        new = (node*) malloc(sizeof(node));
+        new->value = insert_value;
+        new->ptr = crawler;
+        precrawler->ptr = new;
+    }
+}
+
+void delete_node(node** head, int delete_value)
+{
+    node* crawler = *head;
+    node* precrawler = NULL;
+
+    if (crawler != NULL)
+    {
+        while(crawler->value != delete_value)
+        {
+            precrawler = crawler;
+            crawler = crawler->ptr;
+            if (crawler == NULL)
+            {
+                break;
+            }
+        }
+
+        if (crawler != NULL)
+        {
+            if (precrawler == NULL)
+            {
+                *head = crawler->ptr;
+            }
+            else
+            {
+                precrawler->ptr = crawler->ptr;
+            }
+            free(crawler);
+        }
     }
 }
 
